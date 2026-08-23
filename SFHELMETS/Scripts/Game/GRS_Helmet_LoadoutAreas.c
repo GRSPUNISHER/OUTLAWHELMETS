dd0c9101ae0e172327defd366302e927483b461f
GRSPUNISHER <tylerochsner31@gmail.com>
2026-08-23 15:23:13 -0700

    Added helmets to github

---
+++ SFHELMETS/Scripts/Game/GRS_Helmet_LoadoutAreas.c
@@ -0,0 +1,14 @@
+// Helmet-mounted battery pack / counterweight
+class GRS_Helmet_Battery: LoadoutAreaType{};
+
+// ARC-rail or headband hearing protection / headsets
+class GRS_Helmet_EarPro: LoadoutAreaType{};
+
+// IR / visible strobe (Manta, ARSD, etc.)
+class GRS_Helmet_Strobe: LoadoutAreaType{};
+
+// Helmet light (Princeton Charge, MPLS, weapon-light on rail)
+class GRS_Helmet_Light: LoadoutAreaType{};
+
+// Night vision device mount point
+class GRS_Helmet_NVG: LoadoutAreaType{};
